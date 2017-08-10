e883f1e8f2ae0e530896c90f1fe5ac1693ae3193
tomaszpiwowarski <tomasz.piwowarski16@wp.pl>
2017-08-10 11:21:06 +0200

    Whole Product: Added attribute for idea element

--- ProjektGrupowy/Models/Games/WholeProduct/WholeProductBuilder.cs
+++ ProjektGrupowy/Models/Games/WholeProduct/WholeProductBuilder.cs
@@ -16,6 +16,7 @@ namespace ProjektGrupowy.Models.Games.WholeProduct
             _idea = new ElementDefinition("Idea");
 
             _idea.AddAttribute(new AttributeDefinition("Nazwa", new StringType()));
+            _idea.AddAttribute(new AttributeDefinition("Oryginalność", new EnumType(new[] { "Ogromna", "Duża", "Średnia", "Mała" }), isAuto: true));
             _idea.AddAttribute(new AttributeDefinition("Opis", new LongTextType(), isAuto: false, isRequired: false));
 
             _idea.ImageIconId = _images.AddImage("/Resources/Images/anchor.png");
@@ -33,9 +34,12 @@ namespace ProjektGrupowy.Models.Games.WholeProduct
             {
                 containers[i] = new RegionContainer(i*12, i*12, 100-24*i, 100-24*i, RegionContainer.OrientationType.VERTICAL);
                 containers[i].SetAcceptElement(_idea);
+
                 BoardRegion region = new BoardRegion(new Color(0, 0, 0), 0.0f, "Nazwa");
+                region.Attributes.Add(new BoardRegion.Attribute("Oryginalność", ((EnumType)_idea.GetAttribute("Oryginalność").Type).Domain[i]));
                 region.PopupAttribute = "Nazwa";
                 containers[i].AddRegion(region);
+
                 _wholeProduct.Board.AddContainer(containers[i]);
             }
         }
